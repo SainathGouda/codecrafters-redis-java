5473eed876f9259f57fbb81307da0689187f8f19
Sainath Gouda <premg130902@gmail.com>
2025-08-19 21:08:18 +0530

    Responds to multiple pings

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -15,8 +16,13 @@ public class Main {
             serverSocket.setReuseAddress(true);
             // Wait for connection from client.
             clientSocket = serverSocket.accept();
+            InputStream inputStream = clientSocket.getInputStream();
             OutputStream outputStream = clientSocket.getOutputStream();
-            outputStream.write("+PONG\r\n".getBytes());
+
+            while(inputStream.available()>0){
+                outputStream.write("+PONG\r\n".getBytes());
+                outputStream.flush();
+            }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
         } finally {
