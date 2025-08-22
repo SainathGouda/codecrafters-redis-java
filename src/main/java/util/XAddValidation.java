486dc5fb1ad37af41f4c0c49fd82e8571dcf5031
Sainath Gouda <premg130902@gmail.com>
2025-08-22 21:48:36 +0530

    XADD Command Update: Auto-generate complete ID

--- src/main/java/util/XAddValidation.java
+++ src/main/java/util/XAddValidation.java
@@ -6,24 +6,35 @@ import data.StreamCache;
 
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.util.Objects;
 
 public class XAddValidation {
     public String isValid(String streamKey, String entryId, Storage storage, BufferedWriter outputStream) throws IOException {
         StreamCache streamCache = storage.getStreamCache(streamKey);
 
-        String[] xAddId = entryId.split("-");
-        int millisecondsTime = Integer.parseInt(xAddId[0]);
+        long millisecondsTime;
         int sequenceNumber;
-        if (xAddId[1].equals("*")) {
-            int lastSequenceNumber = streamCache.getLastSequenceNumberForMs(millisecondsTime);
+        if (entryId.equals("*")) {
+            millisecondsTime = System.currentTimeMillis();
+            sequenceNumber = 0;
 
-            if (lastSequenceNumber >= 0) {
-                sequenceNumber = lastSequenceNumber + 1;
-            } else {
-                sequenceNumber = (millisecondsTime == 0) ? 1 : 0;
+            if (Objects.equals(streamCache.getLastMillisecondsTime(), millisecondsTime)) {
+                sequenceNumber = streamCache.getLastSequenceNumberForMs(millisecondsTime) + 1;
             }
         } else {
-            sequenceNumber = Integer.parseInt(xAddId[1]);
+            String[] xAddId = entryId.split("-");
+            millisecondsTime = Integer.parseInt(xAddId[0]);
+            if (xAddId[1].equals("*")) {
+                int lastSequenceNumber = streamCache.getLastSequenceNumberForMs(millisecondsTime);
+
+                if (lastSequenceNumber >= 0) {
+                    sequenceNumber = lastSequenceNumber + 1;
+                } else {
+                    sequenceNumber = (millisecondsTime == 0) ? 1 : 0;
+                }
+            } else {
+                sequenceNumber = Integer.parseInt(xAddId[1]);
+            }
         }
 
         if (millisecondsTime <= 0 && sequenceNumber <= 0) {
